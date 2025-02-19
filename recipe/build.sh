66663c26c287f09dd0df828f0aa1fe4a18dee0fd
Ted Ralphs <ted@lehigh.edu>
2025-02-19 10:03:12 -0500

    Adding files for Windows build

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,16 +1,23 @@
 #!/usr/bin/env bash
-# Get an updated config.sub and config.guess
-cp $BUILD_PREFIX/share/gnuconfig/config.* .
-cp $BUILD_PREFIX/share/gnuconfig/config.* ./Clp
 set -e
 
-UNAME="$(uname)"
-export CFLAGS="${CFLAGS} -O3"
-export CXXFLAGS="${CXXFLAGS} -O3"
-export CXXFLAGS="${CXXFLAGS//-std=c++17/-std=c++11}"
+# LIBRARY_PREFIX will only be available on Windows
+if [ ! -z ${LIBRARY_PREFIX+x} ]; then
+    USE_PREFIX=$LIBRARY_PREFIX
+else
+    USE_PREFIX=$PREFIX
+fi
 
-if [ "${UNAME}" == "Linux" ]; then
-    export FLIBS="-lgcc_s -lgcc -lstdc++ -lm"
+if [[ "${target_platform}" == win-* ]]; then
+  EXTRA_FLAGS="--enable-msvc --with-coinutils-lib=${LIBRARY_PREFIX}/lib/libCoinUtils.lib --with-coinutils-incdir=\${LIBRARY_PREFIX_COIN} --with-osi-lib=${LIBRARY_PREFIX}/lib/libOsi.lib --with-osi-incdir=\${LIBRARY_PREFIX_COIN}"
+  BLAS_LIB="${LIBRARY_PREFIX}/lib/cblas.lib"
+  LAPACK_LIB="${LIBRARY_PREFIX}/lib/lapack.lib"
+else
+# Get an updated config.sub and config.guess
+  cp $BUILD_PREFIX/share/gnuconfig/config.* .
+  cp $BUILD_PREFIX/share/gnuconfig/config.* ./Clp
+  BLAS_LIB="-L${PREFIX}/lib -lblas"
+  LAPACK_LIB="-L${PREFIX}/lib -llapack"
 fi
 
 # Use only 1 thread with OpenBLAS to avoid timeouts on CIs.
@@ -19,15 +26,18 @@ fi
 # value at run-time to get the expected amount of parallelism.
 export OPENBLAS_NUM_THREADS=1
 
-WITH_BLAS_LIB="-L${PREFIX}/lib -lblas"
-WITH_LAPACK_LIB="-L${PREFIX}/lib -llapack"
-
 ./configure \
-    --prefix="${PREFIX}" \
-    --exec-prefix="${PREFIX}" \
-    --with-blas-lib="${WITH_BLAS_LIB}" \
-    --with-lapack-lib="${WITH_LAPACK_LIB}" \
-    --enable-gnu-packages
+  --prefix="${USE_PREFIX}" \
+  --exec-prefix="${USE_PREFIX}" \
+  --with-blas-lib="${BLAS_LIB}" \
+  --with-lapack-lib="${LAPACK_LIB}" \
+  ${EXTRA_FLAGS} || cat Clp/config.log
 
 make -j "${CPU_COUNT}"
+
+# Tests are broken without Data folder: https://github.com/coin-or/Osi/issues/184
+#if [[ "$CONDA_BUILD_CROSS_COMPILATION" != "1" ]]; then
+#  make test
+#fi
+
 make install
