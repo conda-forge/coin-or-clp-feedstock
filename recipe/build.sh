d7c0fe03850baf6205af520d24ab037d503b8f54
Ted Ralphs <ted@lehigh.edu>
2025-02-24 10:41:00 -0500

    Try single quotes

--- recipe/build.sh
+++ recipe/build.sh
@@ -9,7 +9,7 @@ else
 fi
 
 if [[ "${target_platform}" == win-* ]]; then
-  EXTRA_FLAGS="--enable-msvc --with-coinutils-lib=\"${LIBRARY_PREFIX}/lib/libCoinUtils.lib ${LIBRARY_PREFIX}/lib/cblas.lib ${LIBRARY_PREFIX}/lib/lapack.lib\" --with-coinutils-incdir=\${LIBRARY_PREFIX_COIN} --with-osi-lib=${LIBRARY_PREFIX}/lib/libOsi.lib --with-osi-incdir=\${LIBRARY_PREFIX_COIN}"
+  EXTRA_FLAGS="--enable-msvc --with-coinutils-lib='${LIBRARY_PREFIX}/lib/libCoinUtils.lib ${LIBRARY_PREFIX}/lib/cblas.lib ${LIBRARY_PREFIX}/lib/lapack.lib' --with-coinutils-incdir=\${LIBRARY_PREFIX_COIN} --with-osi-lib=${LIBRARY_PREFIX}/lib/libOsi.lib --with-osi-incdir=\${LIBRARY_PREFIX_COIN}"
   BLAS_LIB="${LIBRARY_PREFIX}/lib/cblas.lib"
   LAPACK_LIB="${LIBRARY_PREFIX}/lib/lapack.lib"
 else
