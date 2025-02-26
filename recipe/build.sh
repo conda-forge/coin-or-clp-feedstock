a16cab4972fb17254dcbf5080d56da12ce8d5620
Ted Ralphs <ted@lehigh.edu>
2025-02-26 11:25:24 -0500

    Fix typo, but is this what is wrong?

--- recipe/build.sh
+++ recipe/build.sh
@@ -12,7 +12,7 @@ if [[ "${target_platform}" == win-* ]]; then
   COINUTLS_LIB=( --with-coinutils-lib='${LIBRARY_PREFIX}/lib/mkl_intel_ilp64.lib ${LIBRARY_PREFIX}/lib/mkl_sequential.lib ${LIBRARY_PREFIX}/lib/mkl_core.lib ${LIBRARY_PREFIX}/lib/libCoinUtils.lib' )
   COINUTILS_INC=( --with-coinutils-incdir='${LIBRARY_PREFIX_COIN}' )
   OSI_LIB=( --with-osi-lib='${LIBRARY_PREFIX}/lib/libOsi.lib' )
-  OSI_INC=( --with-coinutils-incdir='${LIBRARY_PREFIX_COIN}' )
+  OSI_INC=( --with-osi-incdir='${LIBRARY_PREFIX_COIN}' )
   EXTRA_FLAGS=( --enable-msvc ) 
 else
   # Get an updated config.sub and config.guess (for mac arm and lnx aarch64)
