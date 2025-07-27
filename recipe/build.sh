5343cdd1c262f424dd04d1e42a15c8043b5e749f
Davide Fioriti <fioritidavidesubs@gmail.com>
2025-07-27 17:47:29 +0200

    update build with MD Runtime Library

--- recipe/build.sh
+++ recipe/build.sh
@@ -9,11 +9,11 @@ else
 fi
 
 if [[ "${target_platform}" == win-* ]]; then
-  COINUTILS_LIB=( --with-coinutils-lib='${LIBRARY_PREFIX}/lib/mkl_intel_ilp64.lib ${LIBRARY_PREFIX}/lib/mkl_sequential.lib ${LIBRARY_PREFIX}/lib/mkl_core.lib ${LIBRARY_PREFIX}/lib/libCoinUtils.lib' )
+  COINUTILS_LIB=( --with-coinutils-lib='${LIBRARY_PREFIX}/lib/libCoinUtils.lib' )
   COINUTILS_INC=( --with-coinutils-incdir='${LIBRARY_PREFIX_COIN}' )
   OSI_LIB=( --with-osi-lib='${LIBRARY_PREFIX}/lib/libOsi.lib' )
   OSI_INC=( --with-osi-incdir='${LIBRARY_PREFIX_COIN}' )
-  EXTRA_FLAGS=( --enable-msvc ) 
+  EXTRA_FLAGS=( --enable-msvc=MD ) 
 else
   # Get an updated config.sub and config.guess (for mac arm and lnx aarch64)
   cp $BUILD_PREFIX/share/gnuconfig/config.* ./Clp 
