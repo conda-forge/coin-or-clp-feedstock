5d5bed0e421ce8ddda8a30db01b92106fe6c7ca0
Donato Meoli <donato.meoli.95@gmail.com>
2025-08-02 15:26:01 +0200

    Update build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -9,11 +9,11 @@ else
 fi
 
 if [[ "${target_platform}" == win-* ]]; then
-  COINUTILS_LIB=( --with-coinutils-lib='${LIBRARY_PREFIX}/lib/libCoinUtils.lib' )
+  COINUTILS_LIB=( --with-coinutils-lib='${LIBRARY_PREFIX}/lib/mkl_rt.lib ${LIBRARY_PREFIX}/lib/libCoinUtils.lib' )
   COINUTILS_INC=( --with-coinutils-incdir='${LIBRARY_PREFIX_COIN}' )
   OSI_LIB=( --with-osi-lib='${LIBRARY_PREFIX}/lib/libOsi.lib' )
   OSI_INC=( --with-osi-incdir='${LIBRARY_PREFIX_COIN}' )
-  EXTRA_FLAGS=( --enable-msvc=MD --with-blas-lib='${LIBRARY_PREFIX}/lib/mkl_rt.lib' )
+  EXTRA_FLAGS=( --enable-msvc=MD )
 else
   # Get an updated config.sub and config.guess (for mac arm and lnx aarch64)
   cp $BUILD_PREFIX/share/gnuconfig/config.* ./Clp 
